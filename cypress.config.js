7dd35c427c7d6f5642365000470e72bc1d850c06
Shaishav-code <shd.narola@gmail.com>
2024-08-22 11:44:03 +0530

    lastUpdated

--- cypress.config.js
+++ cypress.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
     defaultCommandTimeout: 20000,
     pageLoadTimeout: 60000,
     retries: {
-      runMode: 3, // Number of retries in 'cypress run' mode
-      openMode: 3, // Number of retries in 'cypress open' mode
+      // runMode: 3, 
+      openMode: 3, 
     },
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.{js,jsx,ts,tsx}',
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,10 +1,9 @@
 Cypress.on('uncaught:exception', (err, runnable) => {
-    // Check for the specific error message
+    // Ignore the specific ResizeObserver error
     if (err.message.includes('ResizeObserver loop completed with undelivered notification')) {
-      // Prevent the error from failing the test
       return false;
     }
-    // Let other errors fail the test
+    // Fail the test on other uncaught exceptions
     return true;
-  });
-  
+   });
+   
